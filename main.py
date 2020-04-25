f46d435a85aa821a15448e9cd95d0415ef6643f2
sirily11 <sirily1997@gmail.com>
2020-04-25 17:53:08 -0500

    Added 7.23

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from patch_fetcher.patch_fetcher import PatchFetcher
 from pprint import pprint
 
 if __name__ == '__main__':
-    p = PatchFetcher(version='7.24')
+    p = PatchFetcher(version='7.23b')
     g = p.fetch()
     pprint(g.to_json())
